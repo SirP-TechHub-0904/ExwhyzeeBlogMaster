7b1f8892374f02b97f424c5c9ea5bf95a4eea670
VS-GitHub-001 <105536514+VS-GitHub-001@users.noreply.github.com>
2025-08-12 10:41:00 +0100

    update-patch-2

--- src/ModelData/Data/ApplicationDbContext.cs
+++ src/ModelData/Data/ApplicationDbContext.cs
@@ -15,7 +15,9 @@ namespace ModelData.Data
             _httpContextAccessor = httpContextAccessor;
         }
         private readonly IHttpContextAccessor _httpContextAccessor;
-        //
+        /// <summary>
+        /// /
+        /// </summary>
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<WebPage> Pages { get; set; }
